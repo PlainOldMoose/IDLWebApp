2d3bb0aed16e104edf3f136dc57330c7916978b5
PlainOldMoose <NotAMoos3@gmail.com>
2026-01-15 21:00:31 +0000

    Updated PlayerService to support DTOs

--- backend/src/main/java/com/plainoldmoose/IDLWebApp/service/PlayerService.java
+++ backend/src/main/java/com/plainoldmoose/IDLWebApp/service/PlayerService.java
@@ -1,25 +1,57 @@
 package com.plainoldmoose.IDLWebApp.service;
 
+import com.plainoldmoose.IDLWebApp.dto.request.CreatePlayerRequest;
+import com.plainoldmoose.IDLWebApp.dto.response.PlayerDetailResponse;
+import com.plainoldmoose.IDLWebApp.dto.response.PlayerSummaryResponse;
 import com.plainoldmoose.IDLWebApp.model.Player;
 import com.plainoldmoose.IDLWebApp.repository.PlayerRepository;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class PlayerService {
 
-    private PlayerRepository playerRepository;
+    private final PlayerRepository playerRepository;
 
-    @GetMapping
-    public List<Player> getAllPlayers() {
-        return playerRepository.findAll();
+    public List<PlayerSummaryResponse> getAllPlayersSummary() {
+        List<PlayerSummaryResponse> playerlist = new ArrayList<>();
+
+        List<Player> players = playerRepository.findAll();
+        for (Player player : players) {
+            playerlist.add(mapToResponse(player));
+        }
+        return playerlist;
     }
 
-    public void insertPlayer(Player player) {
-        playerRepository.save(player);
+    public PlayerDetailResponse getPlayerById(String steamId) {
+        Optional<Player> player = playerRepository.findBySteamId(steamId);
+
+        if (player.isPresent()) {
+            return mapToDetailResponse(player.get());
+        }
+
+        return null;
+    }
+
+    public PlayerSummaryResponse createPlayer(CreatePlayerRequest request) {
+        Player player = new Player();
+        player.setUsername(request.username());
+        player.setElo(request.elo());
+        player.setSteamId(request.steamId());
+        Player saved = playerRepository.save(player);
+        return mapToResponse(saved);
+    }
+
+    private PlayerSummaryResponse mapToResponse(Player player) {
+        return new PlayerSummaryResponse(player.getUsername(), player.getElo(), player.getSteamId());
+    }
+
+    private PlayerDetailResponse mapToDetailResponse(Player player) {
+        return new PlayerDetailResponse(player.getId(), player.getUsername(), player.getElo(), player.getSteamId(), player.getCreatedAt(), player.getEloHistory(), player.getMatchParticipations(), player.getTeamMemberships());
     }
 }
