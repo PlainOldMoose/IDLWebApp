b8b9351075f1dea05915e3405ebcc936fabeabd1
Luque <luque@vandermerwe.uk>
2026-01-15 20:58:02 +0000

    Updated PlayerController to support new DTOs

--- backend/src/main/java/com/plainoldmoose/IDLWebApp/controller/PlayerController.java
+++ backend/src/main/java/com/plainoldmoose/IDLWebApp/controller/PlayerController.java
@@ -1,7 +1,10 @@
 package com.plainoldmoose.IDLWebApp.controller;
 
-import com.plainoldmoose.IDLWebApp.model.Player;
+import com.plainoldmoose.IDLWebApp.dto.request.CreatePlayerRequest;
+import com.plainoldmoose.IDLWebApp.dto.response.PlayerDetailResponse;
+import com.plainoldmoose.IDLWebApp.dto.response.PlayerSummaryResponse;
 import com.plainoldmoose.IDLWebApp.service.PlayerService;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,12 +17,17 @@ public class PlayerController {
     private final PlayerService playerService;
 
     @GetMapping
-    public List<Player> getAllPlayers() {
-        return playerService.getAllPlayers();
+    public List<PlayerSummaryResponse> getAllPlayersSummary() {
+        return playerService.getAllPlayersSummary();
+    }
+
+    @GetMapping("/{steamId}")
+    public PlayerDetailResponse getPlayerById(@PathVariable String steamId) {
+        return playerService.getPlayerById(steamId);
     }
 
     @PostMapping
-    public void addNewPlayer(@RequestBody Player player) {
-        playerService.insertPlayer(player);
+    public PlayerSummaryResponse createPlayer(@Valid @RequestBody CreatePlayerRequest request) {
+        return playerService.createPlayer(request);
     }
 }
