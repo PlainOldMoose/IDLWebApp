347b9747828b5e142a900a548f8159c07e6bcb2d
PlainOldMoose <NotAMoos3@gmail.com>
2026-01-15 20:57:10 +0000

    Implemented Player responses, basic (for listing all players, their elo and their steamID) and detailed (basic + extra statistics such as elo changes and previous teams

--- backend/src/main/java/com/plainoldmoose/IDLWebApp/dto/response/PlayerDetailResponse.java
+++ backend/src/main/java/com/plainoldmoose/IDLWebApp/dto/response/PlayerDetailResponse.java
@@ -1,4 +1,22 @@
 package com.plainoldmoose.IDLWebApp.dto.response;
 
-public record PlayerSummaryResponse(String username, Long elo, String steamId) {
+import com.plainoldmoose.IDLWebApp.model.EloHistory;
+import com.plainoldmoose.IDLWebApp.model.MatchParticipant;
+import com.plainoldmoose.IDLWebApp.model.TeamPlayer;
+import jakarta.annotation.Nullable;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public record PlayerDetailResponse(
+        Long id,
+        String username,
+        Long elo,
+        String steamId,
+        LocalDateTime createdAt,
+        // TODO - Refactor next 3 lines to use DTOs instead of entities
+        @Nullable List<EloHistory> eloHistory,
+        @Nullable List<MatchParticipant> matchParticipations,
+        @Nullable List<TeamPlayer> teamMemberships
+) {
 }
