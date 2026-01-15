3d943b3aa4a9c111c5992432c8b5c8fdae249a35
Luque <luque@vandermerwe.uk>
2026-01-15 18:20:59 +0000

    Implemented TeamPlayer to represent a player on a certain team

--- backend/src/main/java/com/plainoldmoose/IDLWebApp/model/TeamPlayer.java
+++ backend/src/main/java/com/plainoldmoose/IDLWebApp/model/TeamPlayer.java
@@ -1,4 +1,21 @@
 package com.plainoldmoose.IDLWebApp.model;
 
+import com.plainoldmoose.IDLWebApp.model.enums.TeamRole;
+import jakarta.persistence.*;
+
+
+@Entity
 public class TeamPlayer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne
+    private Team team;
+
+    @ManyToOne
+    private Player player;
+
+    @Enumerated(EnumType.STRING)
+    private TeamRole role;
 }
