655639822a93cd7f01948025038a5007b9711e7f
PlainOldMoose <NotAMoos3@gmail.com>
2026-01-15 18:20:18 +0000

    Implemented MatchParticipant class to represent a player in a given match, along with if they are subbing or not

--- backend/src/main/java/com/plainoldmoose/IDLWebApp/model/MatchParticipant.java
+++ backend/src/main/java/com/plainoldmoose/IDLWebApp/model/MatchParticipant.java
@@ -1,4 +1,33 @@
 package com.plainoldmoose.IDLWebApp.model;
 
+import com.plainoldmoose.IDLWebApp.model.enums.Side;
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+
+@Entity
 public class MatchParticipant {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Match match;
+
+    @ManyToOne
+    private Player player;
+
+    @Enumerated
+    private Side side;
+
+    private Long eloBefore;
+
+    private Long eloAfter;
+
+    private Long eloChange;
+
+    private Boolean isSub;
+
+    @ManyToOne
+    @Nullable
+    private Player subbingFor;
 }
