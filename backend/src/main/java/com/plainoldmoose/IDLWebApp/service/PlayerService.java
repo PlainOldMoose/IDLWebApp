f99601ee6eba5cac04c5058bb2012a1ba9724fe4
PlainOldMoose <NotAMoos3@gmail.com>
2026-01-15 17:25:17 +0000

    Adding backend dir to repo

---
+++ backend/src/main/java/com/plainoldmoose/IDLWebApp/service/PlayerService.java
@@ -0,0 +1,26 @@
+package com.plainoldmoose.IDLWebApp.service;
+
+import com.plainoldmoose.IDLWebApp.model.Player;
+import com.plainoldmoose.IDLWebApp.repository.PlayerRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class PlayerService {
+
+    private PlayerRepository playerRepository;
+
+    @GetMapping
+    public List<Player> getAllPlayers() {
+        return playerRepository.findAll();
+    }
+
+    public void insertPlayer(Player player) {
+        playerRepository.save(player);
+    }
+}
---
+++ backend/src/test/java/com/plainoldmoose/IDLWebApp/IdlWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plainoldmoose.IDLWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdlWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
