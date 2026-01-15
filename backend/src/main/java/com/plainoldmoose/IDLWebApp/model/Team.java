b4625ff4bdcf3cb62ed55fb78b1322d5ca31e888
Luque <luque@vandermerwe.uk>
2026-01-15 17:46:50 +0000

    Implemeneted Team class to represent a team within an IDL season

--- backend/src/main/java/com/plainoldmoose/IDLWebApp/model/Team.java
+++ backend/src/main/java/com/plainoldmoose/IDLWebApp/model/Team.java
@@ -1,4 +1,27 @@
 package com.plainoldmoose.IDLWebApp.model;
 
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
 public class Team {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "season_id", nullable = false)
+    private Season season;
+
+    private Long totalElo;
+
+    private int wins;
+    private int losses;
+
+    @OneToMany(mappedBy = "team")
+    private List<TeamPlayer> teamPlayers;
 }
