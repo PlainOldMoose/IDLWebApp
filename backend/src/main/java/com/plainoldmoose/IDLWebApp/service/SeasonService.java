9c7e68922e15212e27428bae992dfaf07b209060
Luque <luque@vandermerwe.uk>
2026-01-15 19:41:47 +0000

    Implemented SeasonService to handle SeasonController calls

--- backend/src/main/java/com/plainoldmoose/IDLWebApp/service/SeasonService.java
+++ backend/src/main/java/com/plainoldmoose/IDLWebApp/service/SeasonService.java
@@ -1,4 +1,25 @@
 package com.plainoldmoose.IDLWebApp.service;
 
+import com.plainoldmoose.IDLWebApp.model.Season;
+import com.plainoldmoose.IDLWebApp.repository.SeasonRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
 public class SeasonService {
+
+    private SeasonRepository seasonRepository;
+
+    @GetMapping
+    public List<Season> getAllSeasons() {
+        return seasonRepository.findAll();
+    }
+
+    public void insertSeason(Season season) {
+        seasonRepository.save(season);
+    }
 }
