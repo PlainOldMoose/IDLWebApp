f1d8bf8db5ab5cf53e4bd2846af93ca2967b0ccb
Luque <luque@vandermerwe.uk>
2026-01-15 17:46:18 +0000

    Implemented Season.java to represent an IDL season

---
+++ backend/src/main/java/com/plainoldmoose/IDLWebApp/model/Season.java
@@ -0,0 +1,27 @@
+package com.plainoldmoose.IDLWebApp.model;
+
+import com.plainoldmoose.IDLWebApp.model.enums.SeasonStatus;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+public class Season {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    private SeasonStatus status;
+
+    private LocalDate startDate;
+    private LocalDate endDate;
+
+    @OneToMany(mappedBy = "season")
+    private List<Team> teams;
+}
