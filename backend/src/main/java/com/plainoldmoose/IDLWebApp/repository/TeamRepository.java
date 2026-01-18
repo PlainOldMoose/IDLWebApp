9f3d0293f46d3d8eb47eb80338beec3c142ccb51
PlainOldMoose <NotAMoos3@gmail.com>
2026-01-18 17:30:59 +0000

    Refactored repos to use UUID and String for STEAMID64 (for players only)

--- backend/src/main/java/com/plainoldmoose/IDLWebApp/repository/TeamRepository.java
+++ backend/src/main/java/com/plainoldmoose/IDLWebApp/repository/TeamRepository.java
@@ -1,4 +1,10 @@
 package com.plainoldmoose.IDLWebApp.repository;
 
-public class TeamRepository {
+import com.plainoldmoose.IDLWebApp.model.Team;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface TeamRepository extends JpaRepository<Team, UUID> {
+
 }
