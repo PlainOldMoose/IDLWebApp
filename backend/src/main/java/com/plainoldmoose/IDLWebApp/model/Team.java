a1f91ec5c2b6e6a161b238e8b061004b8caeb145
Luque <luque@vandermerwe.uk>
2026-01-18 14:44:24 +0000

    Added captain tracking and removed teamplayer tracking from team entity

--- backend/src/main/java/com/plainoldmoose/IDLWebApp/model/Team.java
+++ backend/src/main/java/com/plainoldmoose/IDLWebApp/model/Team.java
@@ -1,10 +1,18 @@
 package com.plainoldmoose.IDLWebApp.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Team {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -17,11 +25,12 @@ public class Team {
     @JoinColumn(name = "season_id", nullable = false)
     private Season season;
 
+    @ManyToOne
+    @JoinColumn(name = "captain_id", nullable = false)
+    private Player captain;
+
     private Long totalElo;
 
     private int wins;
     private int losses;
-
-    @OneToMany(mappedBy = "team")
-    private List<TeamPlayer> teamPlayers;
 }
