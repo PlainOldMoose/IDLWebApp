0ef2604a541cac86d02b174fcf150363e4f1c663
Luque <luque@vandermerwe.uk>
2026-01-18 14:30:42 +0000

    Updated EloHistory to include lombom Getter/Setter

--- backend/src/main/java/com/plainoldmoose/IDLWebApp/model/EloHistory.java
+++ backend/src/main/java/com/plainoldmoose/IDLWebApp/model/EloHistory.java
@@ -3,11 +3,15 @@ package com.plainoldmoose.IDLWebApp.model;
 import com.plainoldmoose.IDLWebApp.model.enums.EloChangeReason;
 import jakarta.annotation.Nullable;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Entity
+@Setter
+@Getter
 public class EloHistory {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,6 +23,7 @@ public class EloHistory {
     @ManyToOne
     private Match match;
 
+    // Elo after change
     @Column(nullable = false)
     private Long elo;
 
