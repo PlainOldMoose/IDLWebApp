fdd8a5265cfcbd88a8abffc13f9a0b4c45c12569
Luque <luque@vandermerwe.uk>
2026-01-18 19:18:43 +0000

    Updated EloHistory to include eloChange field, this will need to be revisited once actual elo calculations are put inplace

--- backend/src/main/java/com/plainoldmoose/IDLWebApp/model/EloHistory.java
+++ backend/src/main/java/com/plainoldmoose/IDLWebApp/model/EloHistory.java
@@ -1,12 +1,10 @@
 package com.plainoldmoose.IDLWebApp.model;
 
 import com.plainoldmoose.IDLWebApp.model.enums.EloChangeReason;
-import jakarta.annotation.Nullable;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Entity
@@ -18,14 +16,19 @@ public class EloHistory {
     private Long id;
 
     @ManyToOne
+    @JoinColumn(name = "player_id")
     private Player player;
 
     @ManyToOne
+    @JoinColumn(name = "match_id")
     private Match match;
 
     // Elo after change
     @Column(nullable = false)
     private Long elo;
+
+    @Column(nullable = false)
+    private Long eloChange;
 
     @Column(nullable = false)
     private LocalDateTime timestamp;
