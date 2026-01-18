e1c0d8434795d5ab347d6cfb9aee0fac6b399e06
Luque <luque@vandermerwe.uk>
2026-01-18 14:31:50 +0000

    Implemented TeamController for /teams endpoint

--- backend/src/main/java/com/plainoldmoose/IDLWebApp/controller/TeamController.java
+++ backend/src/main/java/com/plainoldmoose/IDLWebApp/controller/TeamController.java
@@ -1,4 +1,22 @@
 package com.plainoldmoose.IDLWebApp.controller;
 
+import com.plainoldmoose.IDLWebApp.dto.response.team.TeamResponse;
+import com.plainoldmoose.IDLWebApp.service.TeamService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping(path = "api/teams")
 public class TeamController {
+    private final TeamService teamService;
+
+    @GetMapping
+    public List<TeamResponse> getAllTeams() {
+        return teamService.getAllTeams();
+    }
 }
