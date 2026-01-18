c877a27c66ea6d3db902be62ea3f82f664a1265b
Luque <luque@vandermerwe.uk>
2026-01-18 17:30:59 +0000

    Refactored repos to use UUID and String for STEAMID64 (for players only)

--- backend/src/main/java/com/plainoldmoose/IDLWebApp/repository/PlayerRepository.java
+++ backend/src/main/java/com/plainoldmoose/IDLWebApp/repository/PlayerRepository.java
@@ -7,9 +7,7 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface PlayerRepository extends JpaRepository<Player, Long> {
+public interface PlayerRepository extends JpaRepository<Player, String> {
     Optional<Player> findByUsername(String username);
     boolean existsByUsername(String username);
-    Optional<Player> findBySteamId(String steamId);
-    boolean existsBySteamId(String steamId);
 }
--- backend/src/main/java/com/plainoldmoose/IDLWebApp/repository/SeasonRepository.java
+++ backend/src/main/java/com/plainoldmoose/IDLWebApp/repository/SeasonRepository.java
@@ -5,10 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+import java.util.UUID;
 
 @Repository
-public interface SeasonRepository extends JpaRepository<Season, Long> {
+public interface SeasonRepository extends JpaRepository<Season, UUID> {
     Optional<Season> findByName(String name);
-    Optional<Season> findById(long id);
     Optional<Season> findByStatus(String status);
 }
