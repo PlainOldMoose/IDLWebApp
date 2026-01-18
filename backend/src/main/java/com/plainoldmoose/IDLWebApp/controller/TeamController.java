b18834ebeea3b0774c1a6503e2c28df06dcc7d83
PlainOldMoose <NotAMoos3@gmail.com>
2026-01-18 17:29:56 +0000

    Refactored controllers to use UUID and String for STEAMID64 (for players only)

--- backend/src/main/java/com/plainoldmoose/IDLWebApp/controller/TeamController.java
+++ backend/src/main/java/com/plainoldmoose/IDLWebApp/controller/TeamController.java
@@ -1,13 +1,9 @@
 package com.plainoldmoose.IDLWebApp.controller;
 
-import com.plainoldmoose.IDLWebApp.dto.response.team.TeamResponse;
 import com.plainoldmoose.IDLWebApp.service.TeamService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @AllArgsConstructor
@@ -15,8 +11,8 @@ import java.util.List;
 public class TeamController {
     private final TeamService teamService;
 
-    @GetMapping
-    public List<TeamResponse> getAllTeams() {
-        return teamService.getAllTeams();
-    }
+//    @GetMapping
+//    public List<TeamResponse> getAllTeams() {
+//        return teamService.getAllTeams();
+//    }
 }
---
+++ backend/src/main/java/com/plainoldmoose/IDLWebApp/repository/TeamRepository.java
@@ -0,0 +1,4 @@
+package com.plainoldmoose.IDLWebApp.repository;
+
+public class TeamRepository {
+}
