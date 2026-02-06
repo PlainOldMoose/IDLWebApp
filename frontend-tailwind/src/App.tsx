76f3ecfc90a5b0227e88883caa0497cbd84afbb0
PlainOldMoose <NotAMoos3@gmail.com>
2026-02-06 14:07:20 +0000

    Implemented skeleton pages for Players, Seasons, and Matches

--- frontend-tailwind/src/App.tsx
+++ frontend-tailwind/src/App.tsx
@@ -1,6 +1,9 @@
 import Navbar from "./components/Navbar.tsx";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Landing from "./pages/Landing.tsx";
+import Seasons from "./pages/Seasons.tsx";
+import Players from "./pages/Players.tsx";
+import Matches from "./pages/Matches.tsx";
 
 function App() {
 
@@ -9,6 +12,9 @@ function App() {
             <Navbar/>
             <Routes>
                 <Route path="/" element={<Landing/>}/>
+                <Route path="/players" element={<Players/>}/>
+                <Route path="/seasons" element={<Seasons/>}/>
+                <Route path="/matches" element={<Matches/>}/>
             </Routes>
         </BrowserRouter>
     );
