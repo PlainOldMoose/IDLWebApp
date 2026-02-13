9b51e6bee85bb37c23566fc06ddb048c90e6d1b6
PlainOldMoose <NotAMoos3@gmail.com>
2026-02-13 22:34:58 +0000

    Removed redundant stat card

--- frontend-tailwind/src/components/SeasonStatCard.tsx
+++
@@ -1,7 +0,0 @@
-export default function SeasonStatCard() {
-    return (
-      <div className="rounded-xl p-8 bg-surface-a20">
-          This is a start card
-      </div>
-    );
-}
--- frontend-tailwind/src/pages/Seasons.tsx
+++ frontend-tailwind/src/pages/Seasons.tsx
@@ -1,6 +1,5 @@
 import {useSeasons} from "../services/Queries.ts";
 import SeasonSummaryCard from "../components/SeasonSummaryCard.tsx";
-import SeasonStatCard from "../components/SeasonStatCard.tsx";
 import {useNavigate} from "react-router-dom";
 
 export default function Seasons() {
@@ -13,11 +12,6 @@ export default function Seasons() {
     return (
         <div>
             <div className="text-5xl text-center font-bold my-12">Seasons</div>
-            <div className="grid grid-cols-3 gap-2 mt-4">
-                <SeasonStatCard/>
-                <SeasonStatCard/>
-                <SeasonStatCard/>
-            </div>
 
             {/*Seasons*/}
             <div className="grid grid-cols-3 gap-4 mt-12">
