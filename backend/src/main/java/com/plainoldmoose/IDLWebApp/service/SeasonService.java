427a36d88e9eef1b6ba4b08935a68dab5c4a369c
PlainOldMoose <NotAMoos3@gmail.com>
2026-02-14 16:05:07 +0000

    Removed redundant annotations from service layer, deleted dead code

--- backend/src/main/java/com/plainoldmoose/IDLWebApp/service/SeasonService.java
+++ backend/src/main/java/com/plainoldmoose/IDLWebApp/service/SeasonService.java
@@ -4,7 +4,6 @@ import com.plainoldmoose.IDLWebApp.dto.request.CreateSeasonRequest;
 import com.plainoldmoose.IDLWebApp.dto.response.season.SeasonDetailResponse;
 import com.plainoldmoose.IDLWebApp.dto.response.season.SeasonSummaryResponse;
 import com.plainoldmoose.IDLWebApp.dto.response.team.TeamMemberResponse;
-import com.plainoldmoose.IDLWebApp.dto.response.team.TeamResponse;
 import com.plainoldmoose.IDLWebApp.dto.response.team.TeamSummaryResponse;
 import com.plainoldmoose.IDLWebApp.model.Season;
 import com.plainoldmoose.IDLWebApp.model.Team;
@@ -13,8 +12,6 @@ import com.plainoldmoose.IDLWebApp.repository.SeasonRepository;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -45,40 +42,16 @@ public class SeasonService {
                 .toList();
     }
 
-    @GetMapping("/{id}")
-    public SeasonDetailResponse getSeasonById(@PathVariable UUID id) {
+    public SeasonDetailResponse getSeasonById(UUID id) {
         Optional<Season> season = seasonRepository.findById(id);
 
         return season.map(this::mapToDetailResponse)
                 .orElse(null);
     }
 
-//    public SeasonDetailResponse getActiveSeason() {
-//        Optional<Season> activeSeason = seasonRepository.findByStatus(SeasonStatus.ACTIVE);
-//        return activeSeason.map(this::mapToDetailResponseSortedByWins)
-//                .orElse(null);
-//    }
-
-//    private SeasonDetailResponse mapToDetailResponseSortedByWins(Season season) {
-//        List<TeamResponse> teams = mapToTeamResponse(season.getTeams());
-//        List<TeamResponse> sortedTeams = teams.stream()
-//                .sorted(Comparator.comparingInt(TeamSummaryResponse::wins)
-//                        .reversed()
-//                        .thenComparing(Comparator.comparingDouble(TeamSummaryResponse::winRate)
-//                                .reversed()))
-//                .toList();
-//
-//        return new SeasonDetailResponse(
-//                season.getName(),
-//                season.getId(),
-//                season.getStatus(),
-//                season.getStartDate(),
-//                season.getEndDate(),
-//                sortedTeams,
-//                season.getWinner() != null ? season.getWinner()
-//                        .getName() : null
-//        );
-//    }
+    private SeasonSummaryResponse mapToSummaryResponse(Season season) {
+        return new SeasonSummaryResponse(season.getName(), season.getStatus(), season.getId(), season.getStartDate(), season.getEndDate());
+    }
 
     private SeasonDetailResponse mapToDetailResponse(Season season) {
         return new SeasonDetailResponse(
@@ -90,10 +63,6 @@ public class SeasonService {
                 mapToTeamSummaryResponse(season.getTeams()),
                 season.getWinner() != null ? season.getWinner()
                         .getName() : null);
-    }
-
-    private SeasonSummaryResponse mapToSummaryResponse(Season season) {
-        return new SeasonSummaryResponse(season.getName(), season.getStatus(), season.getId(), season.getStartDate(), season.getEndDate());
     }
 
     private List<TeamSummaryResponse> mapToTeamSummaryResponse(List<Team> teams) {
@@ -110,38 +79,6 @@ public class SeasonService {
                     t.getCaptain()
                             .getUsername(),
                     members
-            );
-
-            teamsResponse.add(response);
-        }
-        return teamsResponse;
-
-    }
-
-    private List<TeamResponse> mapToTeamResponse(List<Team> teams) {
-        ArrayList<TeamResponse> teamsResponse = new ArrayList<>();
-
-        for (Team t : teams) {
-            List<TeamMemberResponse> members = t.getMembers()
-                    .stream()
-                    .map(this::mapToMemberResponse)
-                    .toList();
-
-            int totalGames = t.getWins() + t.getLosses();
-            double winRate = totalGames > 0 ? (double) t.getWins() / totalGames * 100 : 0.0;
-
-            TeamResponse response = new TeamResponse(
-                    t.getTeamId(),
-                    t.getName(),
-                    t.getCaptain()
-                            .getSteamId(),
-                    t.getCaptain()
-                            .getUsername(),
-                    members,
-                    t.getAvgElo(),
-                    t.getWins(),
-                    t.getLosses(),
-                    winRate
             );
             teamsResponse.add(response);
         }
