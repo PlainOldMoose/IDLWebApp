c9d8931683c3c73190157aa5459e7fe3b015de20
PlainOldMoose <luque@vandermerwe.uk>
2026-06-02 16:01:01 +0100

    Chore(MatchController): Updated packege structure

--- backend/src/main/java/com/plainoldmoose/IDLWebApp/controller/match/MatchController.java
+++ backend/src/main/java/com/plainoldmoose/IDLWebApp/controller/match/MatchController.java
@@ -1,14 +1,20 @@
-package com.plainoldmoose.IDLWebApp.controller;
+package com.plainoldmoose.IDLWebApp.controller.match;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.plainoldmoose.IDLWebApp.dto.response.match.MatchDetailResponse;
 import com.plainoldmoose.IDLWebApp.dto.response.match.MatchSummaryResponse;
 import com.plainoldmoose.IDLWebApp.dto.response.match.UpcomingMatchResponse;
 import com.plainoldmoose.IDLWebApp.service.MatchService;
-import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-import java.util.UUID;
+import lombok.AllArgsConstructor;
 
 @RestController
 @AllArgsConstructor
