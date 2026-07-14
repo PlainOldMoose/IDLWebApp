6a49e6f3db82d62c006732d890f2fecb15580faf
PlainOldMoose <NotAMoos3@gmail.com>
2026-07-14 16:57:58 +0100

    Updated incorrect error message

--- backend/src/main/java/com/plainoldmoose/IDLWebApp/dto/request/CreateSeasonRequest.java
+++ backend/src/main/java/com/plainoldmoose/IDLWebApp/dto/request/CreateSeasonRequest.java
@@ -5,7 +5,7 @@ import jakarta.validation.constraints.NotBlank;
 import java.time.LocalDate;
 
 public record CreateSeasonRequest(
-        @NotBlank(message = "Username is required")
+        @NotBlank(message = "Season name is required")
         String name,
         LocalDate startDate,
         LocalDate endDate) {
